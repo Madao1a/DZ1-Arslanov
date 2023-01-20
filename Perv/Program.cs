067f232ac41d07d8195cae1cc3dd0a52679ce94e
MARS <leprcoin@gmail.com>
2023-01-20 22:56:45 +0400

    dz

---
+++ Perv/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите первое число ");
+int number1 = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введите второе число ");
+int number2 = int.Parse (Console.ReadLine ());
+if (number1 == number2 + number2) {Console.Write ("Yes");}
+if (number1 > number2 + number2) {Console.Write ("NO");}
+if (number1 < number2 + number2) {Console.Write ("NO");}
