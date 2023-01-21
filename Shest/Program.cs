1a7fc38d01b4b6bb1b06b4dbb4516efd326b4e5a
MARS <leprcoin@gmail.com>
2023-01-21 09:48:45 +0400

    dz61

--- Shest/Program.cs
+++ Shest/Program.cs
@@ -3,3 +3,4 @@ int numberA = int.Parse (Console.ReadLine());
 int numberB = (numberA % 10);
 int LastNumber = (numberA % 100 - numberB)/10;
 Console.WriteLine (LastNumber);
+// не забыть попробоавть через string
