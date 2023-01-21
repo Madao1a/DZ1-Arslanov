5f98bccea5dc7ce88abbb7f3aebbea6434112bab
MARS <leprcoin@gmail.com>
2023-01-21 13:43:53 +0400

    dz8

---
+++ Vosem/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите первое число ");
+int number = int.Parse (Console.ReadLine ());
+int A = number / 100;
+int C = number % 10;
+//int C = number % 10;
+//int D = (B - C)/10;
+Console.Write ($"{A} {C}");
