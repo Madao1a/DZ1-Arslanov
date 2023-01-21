f1ed0bc449a0fa32935e7254cc50641e87739f70
MARS <leprcoin@gmail.com>
2023-01-21 22:13:40 +0400

    dz9

---
+++ Devyit/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число ");
+int numberA = int.Parse (Console.ReadLine ());
+int numberB = 6;
+if (numberB < numberA) {Console.Write ("Введеное число больше заданного, картность равна 0");}
+if (numberB % numberA == 0) {Console.Write ("Числа кратные");}
+if (numberB / numberA == numberA) {Console.Write (numberA);}
